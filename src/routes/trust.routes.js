1d8eb6f9a12efca6cb118d3e736aaccc0ce1e15a
KS1 <shinehome888@gmail.com>
2026-03-18 00:27:15 -0700

    trust.routes.js

--- src/routes/trust.routes.js
+++ src/routes/trust.routes.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const trustController = require('../controllers/trust.controller');
 
-router.post('/recalculate', trustController.recalculateTrustScore);
-router.get('/score/:smeId', trustController.getTrustScore);
-router.get('/leaderboard', trustController.getLeaderboard);
-router.get('/stats', trustController.getStats); // ← calls controller function
+router.get('/stats', trustController.getStats);
 
 module.exports = router;
